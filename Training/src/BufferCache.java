8ba077623739448c98ad997165d025dffd30f7f8
Vishnu <Vishnu@DESKTOP-93NTII3>
2019-08-13 20:48:02 +0530

    Change-Id: I0000000000000000000000000000000000000000 Buffer cache checks
    Change-Id: I7458de830af3c4576585bd010c45c38daa0c4262

--- Training/src/BufferCache.java
+++ Training/src/BufferCache.java
@@ -6,58 +6,73 @@ import java.util.HashMap;
 
 public class BufferCache {
 	void checkWorkingSet() {
-		String MainChain= "select NXT_REPL from x$kcbwds where CNUM_SET>0";
-		String AuxChain= "select NXT_REPLAX from x$kcbwds where CNUM_SET>0";
-		String FindAddrs="select prv_repl,nxt_repl from x$bh";
-		String tchCount = "select prv_repl,tch from x$bh";
+		String lruChains= "select NXT_REPL,NXT_REPLAX from x$kcbwds where CNUM_SET>0";
+		String BufferCache="select prv_repl,nxt_repl,tch,decode(state,0,'free',1,'xcur',2,'scur',3,'cr', 4,'read',5,'mrec',6,'irec',7,'write',8,'pi', 9,'memory',10,'mwrite',11,'donated', 12,'protected',  13,'securefile', 14,'siop',15,'recckpt', 16, 'flashfree',  17, 'flashcur', 18, 'flashna') STATE from x$bh";
+
 		try {
 			Connection oraCon = DBConnection.getOraSysCon();
 			Statement stmt = oraCon.createStatement();
-			ResultSet rs = stmt.executeQuery(MainChain);
-			Statement pstmt = oraCon.createStatement();
-			ResultSet rs2 = pstmt.executeQuery(FindAddrs);
-			int mainQueue = 0;
-			int auxQueue = 0;
-			HashMap<String,String> asd = new HashMap<>();
-			
-			while (rs2.next()) {
-				asd.put(rs2.getString(1), rs2.getString(2));
-			}
+			ResultSet rs = stmt.executeQuery(BufferCache);
+			Statement stmt2 = oraCon.createStatement();
+			ResultSet rs2 = stmt2.executeQuery(lruChains);
+			HashMap<String,String> mainlruqueue ;
+			HashMap<String,String> auxlruqueue;
+			HashMap<String,String> wholeQueue = new HashMap<>();
 			HashMap<String,Integer> tchcounts = new HashMap<>();
-			rs2 = pstmt.executeQuery(tchCount);
+			HashMap<String,String> bufstates = new HashMap<>();
+			HashMap<String,Integer> mainstates = new HashMap<>();
+			HashMap<String,Integer> auxstates = new HashMap<>();
+			int i = 0;
+			while (rs.next()) {
+				wholeQueue.put(rs.getString(1),rs.getString(2));
+				bufstates.put(rs.getString(1), rs.getString(4));
+				tchcounts.put(rs.getString(1), rs.getInt(3));
+				i++;
+			}
+			HashMap<String,Integer> totalbufagg= new HashMap<>();
+			for (String temp : bufstates.keySet()) {
+				int temp1 = totalbufagg.get(bufstates.get(temp))==null?0:totalbufagg.get(bufstates.get(temp));
+				totalbufagg.put(bufstates.get(temp),1+temp1);
+			}
+			System.out.println("Total Buffers in Buffer cache --> " + i);
+			System.out.println("Total Buffer States in Buffer cache:");
+			for (String temp : totalbufagg.keySet() ) {
+				System.out.println(temp + " --> " + totalbufagg.get(temp));
+			}
 			while (rs2.next()) {
-				tchcounts.put(rs2.getString(1), rs2.getInt(2));
-			}
-			
-			int mainQueuetch = 0;
-			int auxQueuetch = 0;
-			
-			
-			while (rs.next()) {
-				String ChainStart = rs.getString(1);
-				int i = 0;
-				while (asd.get(ChainStart)!=ChainStart) {
-					ChainStart = asd.get(ChainStart);
-					mainQueuetch = mainQueuetch + (tchcounts.get(ChainStart)==null?0:tchcounts.get(ChainStart));
-					i++;
+				mainlruqueue = new HashMap<>();
+				auxlruqueue = new HashMap<>();
+				String mainptr = rs2.getString(1);
+				String auxptr = rs2.getString(2);
+				while (mainptr!=wholeQueue.get(mainptr)) {
+					mainlruqueue.put(mainptr, wholeQueue.get(mainptr));
+					mainptr = wholeQueue.get(mainptr);
 				}
-				mainQueue  = i;
-				System.out.println(rs.getString(1) + " --> Main Queue blocks --> "  + i +" --> TCH Count => " + mainQueuetch);
-			}
-			rs.close();
-			rs = stmt.executeQuery(AuxChain);
-			while (rs.next()) {
-				String ChainStart = rs.getString(1);
+				while (auxptr!=wholeQueue.get(auxptr)) {
+					auxlruqueue.put(auxptr, wholeQueue.get(auxptr));
+					auxptr = wholeQueue.get(auxptr);
+				}
+				System.out.println("Total Buffers in Main LRU --> " + mainlruqueue.keySet().size());
 				
-				int i = 0;
-				while (asd.get(ChainStart)!=ChainStart) {
-					ChainStart = asd.get(ChainStart);
-					auxQueuetch = auxQueuetch + (tchcounts.get(ChainStart)==null?0:tchcounts.get(ChainStart));
-					i++;
+				for (String temp : mainlruqueue.keySet()) {
+					int temp1 = mainstates.get(bufstates.get(temp))==null?0:mainstates.get(bufstates.get(temp));
+					mainstates.put(bufstates.get(temp),1+temp1);
 				}
-				auxQueue  = i;
-				System.out.println(rs.getString(1) + " --> Aux Queue blocks --> "  + i+" --> TCH Count => " + auxQueuetch);
+				for (String temp : mainstates.keySet() ) {
+					System.out.println(temp + " --> " + mainstates.get(temp));
+				}
+				System.out.println("Total Buffers in AUX LRU --> " + auxlruqueue.keySet().size());
+				for (String temp : auxlruqueue.keySet()) {
+					int temp1 = auxstates.get(bufstates.get(temp))==null?0:auxstates.get(bufstates.get(temp));
+					auxstates.put(bufstates.get(temp),1+temp1);
+				}
+				for (String temp : auxstates.keySet() ) {
+					System.out.println(temp + " --> " + auxstates.get(temp));
+				}
+				
 			}
+			
+			
 		}
 		catch(Exception E) {
 			E.printStackTrace();
--- Training/src/Start.java
+++ Training/src/Start.java
@@ -4,6 +4,5 @@ public class Start {
 	public static void main(String[] args) {
 		BufferCache a = new BufferCache();
 		a.checkWorkingSet();
-		
 	}
 }
