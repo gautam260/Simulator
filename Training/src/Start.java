2a85a1561eb4a476569788a0e74f534a016d9f49
Vishnu <Vishnu@DESKTOP-RDRQOLS>
2019-09-14 12:51:50 +0530

    Change-Id: I0000000000000000000000000000000000000000 Memtable
    Change-Id: Ie1458ad384f1fcf004771402dc1ff5aca4daa83f

--- Training/src/Start.java
+++ Training/src/Start.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class Start {
 	public static void main(String[] args) throws InterruptedException {
-		Blevel a = new Blevel();
-		a.start();
+		MemTest a = new MemTest();
+		a.Start();
 	}
 }
