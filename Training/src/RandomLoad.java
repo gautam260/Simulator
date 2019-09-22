e4a5087e0ae9ee7ae7474540296085cb6b52b0ec
Vishnu <Vishnu@DESKTOP-3FOR8MA>
2019-09-23 04:56:40 +0530

    Change-Id: I0000000000000000000000000000000000000000 a few modifications
    Change-Id: I61c6bd671ba182e2a72b975367788e2e65134830

--- Training/src/RandomLoad.java
+++ Training/src/RandomLoad.java
@@ -2,6 +2,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
@@ -10,39 +12,38 @@ public class RandomLoad {
 		try {
 			Connection oraCon = DBConnection.getOraConn();
 			Statement stmt = oraCon.createStatement();
-			/*try {
+			try {
 				String SQL = "drop table RandomLoad";
 				stmt.execute(SQL);
 			}
 			catch(Exception E) {
 				
 			}
-			String SQL = "  create table randomload (roll number, name varchar2(20), mark1 number, mark2 number, mark3 number)  ";
+			String SQL = "  create table randomload (roll number primary key, name varchar2(20), mark1 number not null, mark2 number, mark3 number not null)  ";
 			stmt.execute(SQL);
-			//SQL = "create index RandomLoad_idx on RandomLoad(roll) ";
-			//stmt.execute(SQL);
-			System.out.println("Created Tables and indexes, Starting Load");*/
-			ExecutorService asd = Executors.newFixedThreadPool(400);
+
+			System.out.println("Created Tables and indexes, Starting Load");
+			ExecutorService asd = Executors.newFixedThreadPool(30);
 			int i = 0;
-			/*while (i < 30) {
+			while (i < 30) {
 				asd.submit(new InsertLoad());
 				i++;
 			}
-			i = 0 ;*/
+			i = 0 ;
 			System.out.println("Loading Data... Sleepin for 10 seconds");
-		//	Thread.currentThread().sleep(10000);
-			while (i < 5) {
-				asd.submit(new DeleteLoad());
+			Thread.currentThread().sleep(120000);
+			asd.shutdown();
+			/*
+			while (i < 10) {
 				asd.submit(new UpdateLoad());
 				i++;
-			} /*
+			}
+			
 			while (i < 10) {
 				asd.submit(new SelectLoad());
 				i++;
-			}
-			*/
-			Thread.currentThread().sleep(20000);
-			exit(0);
+			}*/
+			
 			
 		}
 		catch(Exception E) {
@@ -63,12 +64,12 @@ public class RandomLoad {
 				Connection oraCon = DBConnection.getOraConn();
 				PreparedStatement pstmt = oraCon.prepareStatement("insert into RandomLoad (roll, name,mark1,mark2,mark3) values (?,?,?,?,?)");
 				int i = 0;
-				while (i < 10000000) {
+				while (i < 1000000) {
 					pstmt.setInt(1 , oraSequence.nextVal());
 					pstmt.setString(2, OraRandom.randomString(20));
 					pstmt.setInt(3, OraRandom.randomSkewInt(100));
 					pstmt.setInt(4,  OraRandom.randomSkewInt(100));
-					pstmt.setInt(5,  OraRandom.randomSkewInt(100));
+					pstmt.setInt(5,  OraRandom.randomSkewInt(500));
 					
 					pstmt.addBatch();
 					if (i%100000 == 0) {
@@ -78,6 +79,10 @@ public class RandomLoad {
 					i++;
 				}
 				pstmt.close();
+				oraCon.close();
+				  DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
+				   LocalDateTime now = LocalDateTime.now();  
+				   System.out.println(dtf.format(now));  
 			}
 			catch(Exception E) {
 				E.printStackTrace();
@@ -91,9 +96,18 @@ public class RandomLoad {
 				System.out.println("Staring Update Thread -->" + Thread.currentThread().getName());
 				Connection oraCon = DBConnection.getOraConn();
 				PreparedStatement pstmt = oraCon.prepareStatement("Update RandomLoad set name = ? where roll = ? ");
+				String SQL = "select max(roll) from randomload";
+				Statement stmt = oraCon.createStatement();
+				ResultSet rs = stmt.executeQuery(SQL);
+				int maxvalue= 0;
+				while(rs.next()) {
+					maxvalue = rs.getInt(1);
+				}
+				rs.close();
+				stmt.close();
 				int i = 0;
 				while (i < 1000000) {
-					pstmt.setInt(2 , OraRandom.randomUniformInt(125393593));
+					pstmt.setInt(2 , OraRandom.randomUniformInt(maxvalue));
 					pstmt.setString(1, OraRandom.randomString(20));
 					pstmt.executeUpdate();
 					i++;
@@ -110,11 +124,19 @@ public class RandomLoad {
 			try {
 				System.out.println("Staring Select  Thread -->" + Thread.currentThread().getName());
 				Connection oraCon = DBConnection.getOraConn();
-				PreparedStatement pstmt = oraCon.prepareStatement("select mark2 from  RandomLoad where roll =?");
+				String SQL = "select max(roll) from randomload";
+				Statement stmt = oraCon.createStatement();
+				ResultSet rs = stmt.executeQuery(SQL);
+				int maxvalue = 0;
+				while (rs.next()) {
+					maxvalue = rs.getInt(1);
+				}
+				PreparedStatement pstmt = oraCon.prepareStatement("select mark1 from  RandomLoad where roll =?");
+
 				int i = 0;
 				while (i < 1000000) {
-					pstmt.setInt(1, OraRandom.randomUniformInt(125393593));
-					ResultSet rs = pstmt.executeQuery();
+					pstmt.setInt(1, OraRandom.randomUniformInt(maxvalue));
+					 rs = pstmt.executeQuery();
 					while(rs.next()) {
 						rs.getInt(1);
 					}
@@ -138,9 +160,18 @@ public class RandomLoad {
 				System.out.println("Staring Delete Thread -->" + Thread.currentThread().getName());
 				Connection oraCon = DBConnection.getOraConn();
 				PreparedStatement pstmt = oraCon.prepareStatement("delete from RandomLoad where roll = ? ");
+				String SQL = "select max(roll) from randomload";
+				Statement stmt = oraCon.createStatement();
+				ResultSet rs = stmt.executeQuery(SQL);
+				int maxvalue= 0;
+				while(rs.next()) {
+					maxvalue = rs.getInt(1);
+				}
+				rs.close();
+				stmt.close();
 				int i = 0;
 				while (i < 1000000) {
-					pstmt.setInt(1 , OraRandom.randomUniformInt(125393593));
+					pstmt.setInt(1 , OraRandom.randomUniformInt(maxvalue));
 					pstmt.executeUpdate();
 					i++;
 				}
--- Training/src/Start.java
+++ Training/src/Start.java
@@ -1,8 +1,12 @@
-import java.util.ArrayList;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Start {
 	public static void main(String[] args) throws InterruptedException {
-		MemTest a = new MemTest();
-		a.Start();
+		 DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");  
+		   LocalDateTime now = LocalDateTime.now();  
+		   System.out.println(dtf.format(now));
+		RandomLoad a = new RandomLoad();
+		a.createTable();
 	}
 }
